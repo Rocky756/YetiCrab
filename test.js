82e889db720099e2b76337c04de9675125ddc50a
Ruslan <ruslandegtyrev@gmail.com>
2021-09-10 00:13:24 +0300

    delete test

--- test.js
+++
@@ -1,8 +0,0 @@
-const word = 'abc ebd';
-const test = 'Большой куш abc ebd ляляля тра та та';
-const testArr = test.split(' ');
-console.log(word.length);
-console.log(test);
-console.log(test.includes(word));
-
-
